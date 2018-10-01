9539241050c8defd3ad54d1128e63e0a2855ab40
huanxi <1355473748@qq.com>
2018-10-01 16:05:07 +0800

    linux测试

--- src/main/java/xyz/huanxicloud/findjob/controller/VendorController.java
+++ src/main/java/xyz/huanxicloud/findjob/controller/VendorController.java
@@ -1,6 +1,7 @@
 package xyz.huanxicloud.findjob.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 import xyz.huanxicloud.findjob.common.ReturnMessage;
 import xyz.huanxicloud.findjob.pojo.Position;
 import xyz.huanxicloud.findjob.service.positionservice.PositionService;
+import xyz.huanxicloud.findjob.util.AliOSSUtil;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/vendor")
@@ -25,17 +31,17 @@ public class VendorController {
     }
     @PostMapping("/uploadVideo")
     public ReturnMessage uploadVideo(MultipartFile video){
-        return new ReturnMessage(1,"image/head_img/72f3dfac-4fce-488d-b547-b9310c510059.mp4");
-        /*String filename = UUID.randomUUID().toString() + "." + video.getContentType().split("/")[1];
+       // return new ReturnMessage(1,"image/head_img/72f3dfac-4fce-488d-b547-b9310c510059.mp4");
+        String filename = UUID.randomUUID().toString() + "." + video.getContentType().split("/")[1];
         String ossFile = null;
         try {
-            ossFile = AliOSSUtil.uploadLocalFile((FileInputStream) video.getInputStream(), filename, "image/head_img/","f:\\");
+            ossFile = AliOSSUtil.uploadLocalFile((FileInputStream) video.getInputStream(), filename, "image/head_img/","/var/tmp");
         } catch (IOException e) {
             e.printStackTrace();
         }
         if (!StringUtils.isEmpty(ossFile)) {
             return new ReturnMessage(1, ossFile);
         }
-        return new ReturnMessage(1000, "上传失败");*/
+        return new ReturnMessage(1000, "上传失败");
     }
 }
